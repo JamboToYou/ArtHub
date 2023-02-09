041a3633e850017a6b442e56cbb108b79d239abb
Jambo <jambotoyou@gmail.com>
2023-02-09 22:34:30 +0400

    tmp(fe): added basic styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const App = () => {
-	return <h1>Hello React</h1>;
-};
+function App() {
+	return (
+		<>
+			<header className="navigation">
+				<section className="navigation__items justified">
+					<div className="navigation__item">element1</div>
+					<div className="navigation__item">element2</div>
+					<div className="navigation__item">element3</div>
+					<div className="navigation__item">element4</div>
+					<div className="navigation__item">element5</div>
+				</section>
+			</header>
+			<main>
+
+			</main>
+			<footer>
+
+			</footer>
+		</>
+	)
+}
 
 export default App;
